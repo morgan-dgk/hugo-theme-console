869773fc7b7b2aba0d8581df562721c96aca65ce
Morgan Kerle <mdgk95@proton.me>
2024-09-18 18:26:31 +1000

    attach event listener to button on page load

--- static/overrides/js/theme.js
+++ static/overrides/js/theme.js
@@ -1,20 +1,39 @@
 "use strict";
 
-/**
- *  Retrieve user-set theme preference, if any, from localStorage. This overrides the prefers-color-scheme media feature if set.
- *  @returns {string}
- **/
+const getThemePreference = ({ userPreference, sysPreferenceDark }) => {
+  if (userPreference) return userPreference;
+  if (sysPreferenceDark) return "dark";
+  else return "light";
+};
 
-const detectThemePreference = () => {
-  const sysPreferenceDark = window.matchMedia(
-    "(prefers-color-scheme: dark)",
-  ).matches;
-
-  const userPreferenceDark = localStorage.getItem("theme") === "dark";
-
-  if (sysPreferenceDark || userPreferenceDark) {
-    document.documentElement.setAttribute("data-theme", "dark");
+const setUserThemePreference = (userThemePreference) => {
+  try {
+    localStorage.setItem("theme", userThemePreference);
+  } catch (err) {
+    console.warn("Failed to set user preference!");
+    return;
   }
 };
 
-detectThemePreference();
+const setTheme = (theme) => {
+  document.documentElement.setAttribute("data-theme", theme);
+};
+
+const sysPreferenceDark = window.matchMedia(
+  "(prefers-color-scheme: dark)",
+).matches;
+
+const userPreference = localStorage.getItem("theme");
+let currentTheme = getThemePreference({ userPreference, sysPreferenceDark });
+
+setTheme(currentTheme);
+
+addEventListener("load", (event) => {
+  const buttonToggleTheme = document.querySelector("#btn-toggle-theme");
+  buttonToggleTheme.addEventListener("click", (event) => {
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    setUserThemePreference(newTheme);
+    setTheme(newTheme);
+    currentTheme = newTheme;
+  });
+});
